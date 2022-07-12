36f4df6eefeca73ba3b22de728becabd01528bbd
Fedor <fedor_prg@rambler.ru>
2022-07-12 19:48:26 +1000

    Updated Task2

--- Task2.py
+++ Task2.py
@@ -1,24 +1,7 @@
 #Напишите программу для проверки истинности утверждения
 # ¬(X ⋁ Y ⋁ Z) = ¬X ⋀ ¬Y ⋀ ¬Z для всех значений предикат.
 
-def inversion(a):
-    if a==True:
-        a=False
-    else: a=True
-    return a
-def disjunction(a,b):
-    if a==False and b==False:
-        aVb=False
-    else: aVb=True
-    return aVb
-def conjunction(a,b):
-    if a==True and b==True:
-        aAb=True
-    else: aAb=False
-    return aAb
-x = int(input('Значение предикат x='))!=0
-y = int(input('Значение предикат y='))!=0
-z = int(input('Значение предикат z='))!=0
-
-print(inversion(disjunction(disjunction(x,y),z)),"=",conjunction(conjunction(inversion(x),inversion(y)),inversion(z)))
-
+for x in True, False:
+    for y in True, False:
+        for z in True, False:
+           print (f"{x=}  {y=}  {z=} --> {not (x or y or z)==(not x and not y and not z)}")
